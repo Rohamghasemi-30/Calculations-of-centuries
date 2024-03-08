005ad874f00abd3782201470da3e2ecaa31f6cbb
Roham ghasemi <ghasemiroham71@gmail.com>
2024-03-08 10:53:56 +0330

    Computing

---
+++ js/app.js
@@ -0,0 +1,17 @@
+// let Year = Number(propt("Enter the desired year"));
+
+// if(Year == )
+
+
+let year1 = Number(prompt("Please enter your desired year"));
+let year2 =  Number(prompt("Please re-enter your desired year"));
+let number = Number(prompt(" Enter the first two digits of your year"));
+
+function century (year1) {
+    if(year1 - year2 == 0){
+        return number + 1
+    }
+    
+}
+
+alert(century(year1))
